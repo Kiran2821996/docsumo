3e5bef3af60ea15906bdd4087749991f895ecee8
Kiran2821996 <kirandchennai@gmail.com>
2023-01-12 23:24:10 +0530

    add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { EyeOutlined, EyeInvisibleOutlined,LoadingOutlined } from "@ant-design/icons";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
 function Form({ setSuccess, setUser }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordShown, setPasswordShown] = useState(false);
-  const [validateError,setValidateError]=useState({})
+  const [validateError, setValidateError] = useState({});
   const [error, setError] = useState("");
   const [toggle, setToggle] = useState(true);
   let recievedData;
@@ -17,13 +20,14 @@ function Form({ setSuccess, setUser }) {
 
   //form validation
   const validate = () => {
-    const newErrors = {};
+    const newErrors = {
+      email: "",
+      password: "",
+    };
 
     if (!email) {
       newErrors.email = "Please enter a valid email address";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
-    ) {
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
       newErrors.email = "Invalid email address";
     }
 
@@ -33,24 +37,27 @@ function Form({ setSuccess, setUser }) {
       newErrors.password = "Password must be at least 8 characters long";
     }
 
-
     return newErrors;
   };
-
 
   //handle form submit
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
     setError("");
     const newErrors = validate();
-    if(!validateError.email || !validateError.password){
-      console.log("error",validateError.email)
+    if (
+      newErrors &&
+      (newErrors.email.length > 0 || newErrors.password.length > 0)
+    ) {
       setToggle(true);
       setValidateError(newErrors);
-    }else{
+    } else {
       try {
         setToggle(false);
+        setValidateError({
+          email: "",
+          password: "",
+        });
         const response = await fetch(
           "https://apptesting.docsumo.com/api/v1/eevee/login/",
           {
@@ -75,25 +82,35 @@ function Form({ setSuccess, setUser }) {
         setError(error.message);
       }
     }
-    
-    
   };
 
   return (
-    <form onSubmit={handleSubmit} className={validateError.email ||validateError.password?"form1":"form"}>
+    <form
+      onSubmit={handleSubmit}
+      className={
+        validateError.email && validateError.password ? "form1" : "form"
+      }
+      id={validateError.email || validateError.password ? "form1" : "form"}
+    >
       <h3>Login to your Docsumo account</h3>
-      {error ? <p className="apiError">{error}</p>:<div className="none"></div>}
+      {error ? (
+        <p className="apiError">{error}</p>
+      ) : (
+        <div className="none"></div>
+      )}
       <label htmlFor="email">
         <p className="label">Work Email</p>
         <input
           type="email"
           name="email"
-          className={validateError.email?"emailValidate":"email_input"}
+          className={validateError.email ? "emailValidate" : "emailInput"}
           placeholder="johndoe@abc.com"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-         {validateError.email && <p className="validError">{validateError.email}</p>}
+        {validateError.email && (
+          <p className="validError">{validateError.email}</p>
+        )}
       </label>
       <br />
 
@@ -103,29 +120,55 @@ function Form({ setSuccess, setUser }) {
           type={passwordShown ? "text" : "password"}
           name="password"
           placeholder="Enter password here..."
-          className={validateError.password?"passwordValidate":"password_input"}
+          className={
+            validateError.password ? "passwordValidate" : "passwordInput"
+          }
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         {passwordShown ? (
-            <EyeOutlined className={validateError.email ||validateError.password?"eye1":"eye"} onClick={visiblity} />
-         
+          <VisibilityOutlinedIcon
+            id={
+              (validateError.email && validateError.password && "eye1") ||
+              (validateError.email && "eye2") ||
+              (validateError.password && "eye3") ||
+              (!validateError.email && !validateError.password && "eye")
+            }
+            onClick={visiblity}
+          />
         ) : (
-            <EyeInvisibleOutlined className={validateError.email ||validateError.password?"eye1":"eye"} onClick={visiblity}  />
+          <VisibilityOffOutlinedIcon
+            id={
+              (validateError.email && validateError.password && "eye1") ||
+              (validateError.email && "eye2") ||
+              (validateError.password && "eye3") ||
+              (!validateError.email && !validateError.password && "eye")
+            }
+            onClick={visiblity}
+          />
         )}
-        {validateError.password && <p className="validError">{validateError.password }</p>}
+
+        {validateError.password && (
+          <p className="validError">{validateError.password}</p>
+        )}
       </label>
-      <div className="for_pass">
-        <span>Forgot Password?</span>
+      <div className="forPass">
+        <p>Forgot Password?</p>
       </div>
-      {toggle?<button type="submit" className= "sign_up">
-        Login
-      </button>:<button type="submit" className= "sign_up_logging">
-      < ><LoadingOutlined /> Logging in...</> 
-      </button>}
-      
-      <h2 className="sig-up">
-        Don't have an account? <span className="sig-up-col"> Sign Up</span>
+      {toggle ? (
+        <button type="submit" className="signUp">
+          Login
+        </button>
+      ) : (
+        <button type="submit" className="signUpLogging">
+          <>
+            <LoadingOutlined /> Logging in...
+          </>
+        </button>
+      )}
+
+      <h2 className="sigUp">
+        Don't have an account? <span className="sigUpCol"> Sign Up</span>
       </h2>
     </form>
   );
--- src/components/UserName.jsx
+++ src/components/UserName.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function UserName({user}) {
   return (
-   <h2 className="user_name" >Hello "{user}"</h2>
+   <h2 className="userName" >Hello {user}</h2>
   )
 }
 
-export default UserName;
+export default UserName
